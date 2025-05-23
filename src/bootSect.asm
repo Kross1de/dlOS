b54536428298f213784c4ebad3268854fc84c5cf
Kross1de <awesome.hubygu@yandex.ru>
2025-05-23 21:16:06 +0600

    Printing 'T' to the screen

--- src/bootSect.asm
+++ src/bootSect.asm
@@ -1,9 +1,14 @@
-;;; Basic Boot Sector that will jump continuously
+;;; 
+;;; Simple Boot Sector that prints a character using BIOS int 0x10/ AH 0x0e
 ;;;
+
+	mov ah, 0x0e		; int 10/ ah 0x0e BIOS teletype output
+	mov al, 'T'		; character we want to print
+	int 0x10		; BIOS video interrupts
 
 here:
 	jmp here		; jump repeatedly to label 'here'; neverending
-
+	
 	times 510-($-$$) db 0	; pads out 0s until we reach 510th byte
 
 	dw 0xaa55		; BIOS magic number; BOOT magic number
