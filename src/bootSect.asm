0c0d503de7a2c5f06ff592304f3f6fc04b127394
Kross1de <awesome.hubygu@yandex.ru>
2025-05-23 22:10:08 +0600

    change color & video mode

--- src/bootSect.asm
+++ src/bootSect.asm
@@ -5,7 +5,13 @@
 
 	;; Set video mode
 	mov ah, 0x00		; int 0x10 ah 0x00 = set video mode
-	mov al, 0x03		; 80x25 text mode
+	mov al, 0x01		; 40x25 text mode
+	int 0x10
+
+	;; Change color/Palette
+	mov ah, 0x0B
+	mov bh, 0x00
+	mov bl, 0x01
 	int 0x10
 
 	;; Tele-type output
