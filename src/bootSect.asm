ad4b7671de48288b39d9f7281379a355e7f63622
Kross1de <awesome.hubygu@yandex.ru>
2025-05-23 21:29:54 +0600

    minor changes

--- src/bootSect.asm
+++ src/bootSect.asm
@@ -1,20 +1,17 @@
 ;;; 
 ;;; Simple Boot Sector that prints a character using BIOS int 0x10/ AH 0x0e
 ;;;
+	org 0x7c00		; 'origin' of Boot code; helps make sure addresses don't change
 
 	mov ah, 0x0e		; int 10/ ah 0x0e BIOS teletype output
-	mov al, 'T'		; character we want to print
+	mov al, [testString] 	; character we want to print; memory at address testString
 	int 0x10		; BIOS video interrupts
 
-	mov al, 'E'
-	int 0x10
-	mov al, 'S'
-	int 0x10
-	mov al, 'T'
-	int 0x10
 
-here:
-	jmp here		; jump repeatedly to label 'here'; neverending
+
+testString:	db 'T', 0 	; 0/null to null terminate
+
+	jmp $			; keep jumping to here; neverending loop
 	
 	times 510-($-$$) db 0	; pads out 0s until we reach 510th byte
 
