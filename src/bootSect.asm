55a9b3e087f4624836afa2c8bf76fd4d293462d4
Kross1de <awesome.hubygu@yandex.ru>
2025-05-23 21:23:12 +0600

    Printing 'TEST' to the screen

--- src/bootSect.asm
+++ src/bootSect.asm
@@ -1,9 +1,17 @@
 ;;; 
 ;;; Simple Boot Sector that prints a character using BIOS int 0x10/ AH 0x0e
 ;;;
-	
-	mov ax, 0x0e54		; prints a 'T'?
+
+	mov ah, 0x0e		; int 10/ ah 0x0e BIOS teletype output
+	mov al, 'T'		; character we want to print
 	int 0x10		; BIOS video interrupts
+
+	mov al, 'E'
+	int 0x10
+	mov al, 'S'
+	int 0x10
+	mov al, 'T'
+	int 0x10
 
 here:
 	jmp here		; jump repeatedly to label 'here'; neverending
