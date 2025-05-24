1ce9fce36c96ffc4899aca19addbe89ff8c38e0c
Kross1de <awesome.hubygu@yandex.ru>
2025-05-24 22:35:02 +0600

    DISK READ + FIRST KERNEL AFTER PAIN++

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # DlOS Information
 An operating system maded for learn the OS stuff
 
+# Current features
+- Print to screen
+- Basic bootloader
+- Reading from disk
+- "Hello World" kernel
+
 # Build/run instruction
 - To compile the boot sector use:
 ```sh
 fasm bootSect.asm
+fasm kernel.asm
+cat bootSect.bin kernel.bin > OS.bin
 ```
-- To run the boot sector use (qemu example, you can use your vm):
+- To run the operating system use (qemu example, you can use your vm):
 ```sh
-qemu-system-i386 bootSect.bin
+qemu-system-i386 -fda OS.bin
 ```
--- src/bootSect.asm
+++ src/bootSect.asm
@@ -1,43 +1,41 @@
-;;; 
-;;; Simple Boot Sector that prints characters using BIOS interrupts
 ;;;
-	org 0x7c00		; 'origin' of Boot code; helps make sure addresses don't changei
+;;; Simple Boot loader that uses INT13 AH2 to read from disk into memory
+;;;
+	org 0x7c00 		; 'origin' of Boot code; helps make sure addresses don't change
 
-	;; Set video mode
-	mov ah, 0x00		; int 0x10 ah 0x00 = set video mode
-	mov al, 0x01		; 40x25 text mode
-	int 0x10
+	;; set up ES:BX memory address/segment:offset to load sector(s) into
+	mov bx, 0x1000		; load sector to memory address 0x1000
+	mov es, bx
+	mov bx, 0x0 		; ES:BX = 0x1000:0x0
 
-	;; Change color/Palette
-	mov ah, 0x0B
-	mov bh, 0x00
-	mov bl, 0x01
-	int 0x10
+	;; Set up disk read
+	mov dh, 0x0 		; head 0
+	mov dl, 0x0 		; drive 0
+	mov ch, 0x0 		; cylinder 0
+	mov cl, 0x02 		; starting sector to read from disk
 
-	;; Tele-type output
-	mov ah, 0x0e		; int 10/ ah 0x0e BIOS teletype output
-	
-	mov bx, testString	; moving memory address at testString into BX register
-	call print_string
-	
-	mov bx, string2
-	call print_string
+read_disk:
+	mov ah, 0x02 		; BIOS int 13h/ah=2 read disk sectors
+	mov al, 0x01 		; # of sectors to read
+	int 0x13 		; BIOS interrupts for disk functions
 
-	mov dx, 0x12AB
-	call print_hex
+	jc read_disk 		; retry if disk read error (carry flag set/ = 1)
 
-	;; End pgm
-	jmp $			; keep jumping to here; neverending loop
-	
+	;; reset segment registers for RAM
+	mov ax, 0x1000
+	mov ds, ax 		; data segment
+	mov es, ax 		; extra segment
+	mov fs, ax 		; ""
+	mov gs, ax		; ""
+	mov ss, ax		; stack segment
+
+	jmp 0x1000:0x0 		; never return from this!
+
 	;; Included files
 	include 'printStr.asm'
-	include 'printHex.asm'
-	
-	;; Variables
-testString:	db 'Char test: Testing', 0xA, 0xD, 0 	; 0/null to null terminate
-string2:	db 'Hex test: ', 0
+	include 'diskLoad.asm'
 
 	;; Boot sector magic
 	times 510-($-$$) db 0	; pads out 0s until we reach 510th byte
 
-	dw 0xaa55		; BIOS magic number in 511th and 512th bytes
+	dw 0xaa55 		; BIOS magic number; BOOT magic #
